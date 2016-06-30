3cfeca67d6c978b2f4b4d1fafb303bbe432d418e
Roy Hyunjin Han <rhh@crosscompute.com>
2016-06-29 22:51:16 -0400

    Register package in website.dependencies

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,4 @@
-0.0.2
------
-- Ignore multiple clicks on upload button for a few seconds
-
-0.0.1
------
+0.0
+---
 - Add basic upload functionality
+- Ignore multiple clicks on upload button for a few seconds
--- invisibleroads_uploads/__init__.py
+++ invisibleroads_uploads/__init__.py
@@ -4,11 +4,14 @@ from .views import add_routes
 
 
 def includeme(config):
-    settings = config.registry.settings
-    http_expiration_time = get_http_expiration_time(settings)
+    configure_assets(config)
+    add_routes(config)
 
+
+def configure_assets(config):
+    settings = config.registry.settings
+    settings['website.dependencies'].append(config.package_name)
+    http_expiration_time = get_http_expiration_time(settings)
     config.add_static_view(
         '_/invisibleroads-uploads', 'invisibleroads-uploads:assets',
         cache_max_age=http_expiration_time)
-
-    add_routes(config)
