22c70f6684094612b4fabfbc9a9b1ddb45e159f6
Roy Hyunjin Han <rhh@crosscompute.com>
2017-07-09 15:53:21 -0400

    Use new set_default

--- invisibleroads_uploads/__init__.py
+++ invisibleroads_uploads/__init__.py
@@ -1,4 +1,4 @@
-from invisibleroads_macros.iterable import set_default
+from invisibleroads_macros.configuration import set_default
 from invisibleroads_posts import add_website_dependency
 
 from .views import add_routes
