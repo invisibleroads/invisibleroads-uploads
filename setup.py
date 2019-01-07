83ea56c9d083f81df4966ebb04a9cd03405278f7
Roy Hyunjin Han <rhh@crosscompute.com>
2019-01-07 01:16:13 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
     'README.rst', 'CHANGES.rst'])
 setup(
     name='invisibleroads-uploads',
-    version='0.4.2.3',
+    version='0.4.2.4',
     description='File upload functionality',
     long_description=DESCRIPTION,
     classifiers=[
@@ -26,7 +26,7 @@ setup(
         'pytest-runner',
     ],
     install_requires=[
-        'invisibleroads-macros>=0.9.5',
+        'invisibleroads-macros>=0.9.5.1',
         'invisibleroads-posts>=0.6.1',
         'pyramid',
         'pytest',
